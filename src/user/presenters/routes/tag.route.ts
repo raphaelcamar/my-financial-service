fe60450b0f8a3020f3496cfb519fab6497c9a689
Raphael Camargo <raphaelcamar@outlook.com>
2023-07-07 18:54:33 -0300

    fix validation tag

--- src/user/presenters/routes/tag.route.ts
+++ src/user/presenters/routes/tag.route.ts
@@ -1,11 +1,11 @@
 import { GetWalletFromHeaders, VerifyAccessTokenMiddleware } from "@core/generic/infra/middlewares"
 import { App } from "@main/handlers"
-import { TagController } from "@tag/presenters/controllers"
+import { TagController } from "@user/presenters/controllers"
 
 const middlewareToken = new VerifyAccessTokenMiddleware()
-const transactionController = new TagController()
+const tagController = new TagController()
 const getWalletFromHeaders = new GetWalletFromHeaders()
 
 export const TagRoutes = (app: App) => {
-  app.post("/v2/tag", middlewareToken.verify, getWalletFromHeaders.apply, transactionController.create)
+  app.post("/v2/tag", middlewareToken.verify, getWalletFromHeaders.apply, tagController.create)
 }
