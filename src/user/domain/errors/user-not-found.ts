e635a9b0584a88448f60b0ecf93b1d80fcb1ccd7
Raphael Camargo <raphaelcamar@outlook.com>
2024-09-09 16:08:51 -0300

    fix email request statusCode

--- src/user/domain/errors/user-not-found.ts
+++ src/user/domain/errors/user-not-found.ts
@@ -1,7 +1,7 @@
 import { ErrorStatus } from "@core/generic/domain/entities"
 
 export class NotFoundUserError extends Error {
-  public status = ErrorStatus.UNAUTHORIZED
+  public status = ErrorStatus.NOT_FOUND
 
   constructor() {
     super("Usuário não encontrado.")
